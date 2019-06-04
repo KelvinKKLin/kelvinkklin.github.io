c2287cffaa612ff0a59a4aa74b388c1d5f1a9744
Kelvin Lin <kelvinkklin@gmail.com>
2019-06-03 21:06:24 -0400

    rebuilding site Mon, Jun  3, 2019  9:06:24 PM

---
+++ scripts/open_awards.js
@@ -0,0 +1,6 @@
+// When the user clicks on "View Certificate" in the About section, open the corresponding Awards collapsible.
+if(window.location.hash == "#professional-awards"){
+    $("#professional-awards").collapse("show");
+} else if(window.location.hash == "#programming-competition-awards"){
+    $("#programming-competition-awards").collapse("show");
+}
