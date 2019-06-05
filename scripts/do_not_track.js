3e12057fb7973187e6a489aaebb56e8122a9fcbf
Kelvin Lin <kelvinkklin@gmail.com>
2019-06-05 10:56:58 -0400

    Added Google Analytics capabilities

---
+++ scripts/do_not_track.js
@@ -0,0 +1,30 @@
+if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
+    if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
+        // Do Not Track is enabled. Do not load analytics script.
+    } else {
+        // Do Not Track is disabled. Load analytics script.
+        document.head.innerHTML += `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141532868-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'UA-141532868-1');
+            </script>
+        `
+    }
+} else {
+    document.head.innerHTML += `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141532868-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'UA-141532868-1');
+            </script>
+        `
+}
