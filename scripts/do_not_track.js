af6e55cfb2e6740342e8b50a5848fa3970982018
Kelvin Lin <kelvinkklin@gmail.com>
2019-06-05 12:05:52 -0400

    Updated Google Analytics Script

--- scripts/do_not_track.js
+++ scripts/do_not_track.js
@@ -1,32 +1,39 @@
+// Import Google Tag Manager script.
+function generateGoogleImportTagScripts(){
+    var googleImportTagScript = document.createElement("script");
+    googleImportTagScript.async = true;
+    googleImportTagScript.src = "https://www.googletagmanager.com/gtag/js?id=UA-141532868-1";
+    return googleImportTagScript;
+}
+
+// Generate Google Analytics script.
+function generateGoogleAnalyticsScript(){
+    var loadGoogleAnalyticsScript = document.createElement("script");
+    loadGoogleAnalyticsScript.type = "text/javascript";
+    loadGoogleAnalyticsScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-141532868-1');
+    `;
+    return loadGoogleAnalyticsScript;
+}
+
+// Attach Google Analytics scripts to the header.
+function attachHeaders(){
+    document.getElementsByTagName("head")[0].appendChild(generateGoogleImportTagScripts());
+    document.getElementsByTagName("head")[0].appendChild(generateGoogleAnalyticsScript());
+}
+
+// Here is the "DO NOT TRACK" parsing logic.
 if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
     if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
         // Do Not Track is enabled. Do not load analytics script.
     } else {
         // Do Not Track is disabled. Load analytics script.
-        document.head.appendChild(`
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141532868-1"></script>
-            <script>
-            console.log("Google Analytics Enabled");
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', 'UA-141532868-1');
-            </script>
-        `);
+        attachHeaders();
     }
 } else {
-    document.head.appendChild(`
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141532868-1"></script>
-            <script>
-            console.log("Google Analytics Enabled");
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', 'UA-141532868-1');
-            </script>
-        `);
+    attachHeaders();
 }
