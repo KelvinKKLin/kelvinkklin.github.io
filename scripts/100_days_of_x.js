19574ab1bb00d033468d546695bc38ae9207e81b
Kelvin Lin <KelvinKKLin@gmail.com>
2022-12-18 01:43:56 -0800

    rebuilding site Sun, Dec 18, 2022  1:43:56 AM

---
+++ scripts/100_days_of_x.js
@@ -0,0 +1,599 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#day1Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn2").click(function(){
+        $("#day2Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn3").click(function(){
+        $("#day3Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn4").click(function(){
+        $("#day4Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn5").click(function(){
+        $("#day5Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn6").click(function(){
+        $("#day6Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn7").click(function(){
+        $("#day7Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn8").click(function(){
+        $("#day8Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn9").click(function(){
+        $("#day9Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn10").click(function(){
+        $("#day10Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn11").click(function(){
+        $("#day11Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn12").click(function(){
+        $("#day12Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn13").click(function(){
+        $("#day13Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn14").click(function(){
+        $("#day14Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn15").click(function(){
+        $("#day15Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn16").click(function(){
+        $("#day16Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn17").click(function(){
+        $("#day17Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn18").click(function(){
+        $("#day18Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn19").click(function(){
+        $("#day19Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn20").click(function(){
+        $("#day20Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn21").click(function(){
+        $("#day21Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn22").click(function(){
+        $("#day22Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn23").click(function(){
+        $("#day23Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn24").click(function(){
+        $("#day24Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn25").click(function(){
+        $("#day25Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn26").click(function(){
+        $("#day26Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn27").click(function(){
+        $("#day27Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn28").click(function(){
+        $("#day28Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn29").click(function(){
+        $("#day29Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn30").click(function(){
+        $("#day30Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn31").click(function(){
+        $("#day31Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn32").click(function(){
+        $("#day32Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn33").click(function(){
+        $("#day33Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn34").click(function(){
+        $("#day34Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn35").click(function(){
+        $("#day35Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn36").click(function(){
+        $("#day36Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn37").click(function(){
+        $("#day37Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn38").click(function(){
+        $("#day38Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn39").click(function(){
+        $("#day39Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn40").click(function(){
+        $("#day40Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn41").click(function(){
+        $("#day41Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn42").click(function(){
+        $("#day42Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn43").click(function(){
+        $("#day43Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn44").click(function(){
+        $("#day44Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn45").click(function(){
+        $("#day45Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn46").click(function(){
+        $("#day46Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn47").click(function(){
+        $("#day47Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn48").click(function(){
+        $("#day48Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn49").click(function(){
+        $("#day49Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn50").click(function(){
+        $("#day50Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn51").click(function(){
+        $("#day51Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn52").click(function(){
+        $("#day52Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn53").click(function(){
+        $("#day53Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn54").click(function(){
+        $("#day54Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn55").click(function(){
+        $("#day55Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn56").click(function(){
+        $("#day56Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn57").click(function(){
+        $("#day57Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn58").click(function(){
+        $("#day58Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn59").click(function(){
+        $("#day59Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn60").click(function(){
+        $("#day60Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn61").click(function(){
+        $("#day61Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn62").click(function(){
+        $("#day62Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn63").click(function(){
+        $("#day63Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn64").click(function(){
+        $("#day64Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn65").click(function(){
+        $("#day65Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn66").click(function(){
+        $("#day66Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn67").click(function(){
+        $("#day67Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn68").click(function(){
+        $("#day68Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn69").click(function(){
+        $("#day69Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn70").click(function(){
+        $("#day70Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn71").click(function(){
+        $("#day71Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn72").click(function(){
+        $("#day72Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn73").click(function(){
+        $("#day73Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn74").click(function(){
+        $("#day74Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn75").click(function(){
+        $("#day75Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn76").click(function(){
+        $("#day76Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn77").click(function(){
+        $("#day77Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn78").click(function(){
+        $("#day78Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn79").click(function(){
+        $("#day79Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn80").click(function(){
+        $("#day80Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn81").click(function(){
+        $("#day81Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn82").click(function(){
+        $("#day82Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn83").click(function(){
+        $("#day83Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn84").click(function(){
+        $("#day84Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn85").click(function(){
+        $("#day85Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn86").click(function(){
+        $("#day86Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn87").click(function(){
+        $("#day87Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn88").click(function(){
+        $("#day88Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn89").click(function(){
+        $("#day89Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn90").click(function(){
+        $("#day90Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn91").click(function(){
+        $("#day91Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn92").click(function(){
+        $("#day92Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn93").click(function(){
+        $("#day93Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn94").click(function(){
+        $("#day94Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn95").click(function(){
+        $("#day95Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn96").click(function(){
+        $("#day96Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn97").click(function(){
+        $("#day97Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn98").click(function(){
+        $("#day98Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn99").click(function(){
+        $("#day99Modal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn100").click(function(){
+        $("#day100Modal").modal();
+    });
+});
